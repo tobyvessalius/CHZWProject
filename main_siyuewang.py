522249d508d7033c6f35929be5f3d3243165616b
VictoriaWSY <siyuew@bu.edu>
2016-11-21 22:25:32 -0500

    data processing- version 2- Siyue Wang

---
+++ main_siyuewang.py
@@ -0,0 +1,23 @@
+import os, re
+from multiprocessing import Pool
+from rebuild_siyuewang import rebuild
+
+def main(cur):
+    for fname in os.listdir(cur):
+        if os.path.isfile(os.path.join(cur, fname)) and re.search('csv$',fname):
+            rebuild(cur, fname)
+
+if __name__ == '__main__':
+    cur = os.getcwd()
+    if not os.path.exists(os.path.join(cur, 'data/')):
+        os.makedirs(os.path.join(cur, 'data/'))
+
+    directory = []
+    for x in os.listdir('./'):
+        if os.path.isdir(os.path.join(cur, x)) and re.search('set', x):
+            directory.append(x)
+    pool = Pool(processes=len(directory))
+    pool.map(main, directory)
+    pool.close();
+    pool.join();
+    print('Finished!')
